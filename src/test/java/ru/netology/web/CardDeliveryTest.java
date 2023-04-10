f01af02b7eabbe51acc32fb438e43562d385a415
Paul White <paratrak@list.ru>
2023-04-10 21:45:06 +0300

    Code fix #3

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,6 +1,5 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +8,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -18,34 +17,25 @@ class CardDeliveryTest {
     @BeforeEach
     void setUpEach() {
         open("http://localhost:9999");
-
-
     }
 
-
-    static void setUp() {
-        Configuration.headless = true;
-
-    }
-
-    public String dateCreate(long addDays, String pattern) {
+    public String createDate(long addDays, String pattern) {
         return
                 LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
 
     @Test
     void shouldPositiveTestForm() {
         $x("//input[@placeholder=\"Город\"]").setValue("Рязань");
         $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys("BACKSPACE");
-        String meetingDate = dateCreate(5, "dd.MM.yyyy");
+        String meetingDate = createDate(5, "dd.MM.yyyy");
         $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(meetingDate);
         $x("//input[@name]").sendKeys("Алибабаев Василий");
         $x("//input[@name=\"phone\"]").sendKeys("+79269998877");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).should(appear, Duration.ofSeconds(15));
-
+        $("[data-test-id=\"notification\"").should(appear, Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldHave(visible);
     }
 }
 
